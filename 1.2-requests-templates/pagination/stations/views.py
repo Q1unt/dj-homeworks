a9a2640d8d611946554d9330043ba1ed213ed4ee
Q1unt <dima.kunitsien.03@gmail.com>
2023-06-07 16:49:27 +0300

    finalized task 1.2

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,5 @@
+import csv
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,11 +9,13 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
+    with open('data-398-2018-08-30.csv', 'r', encoding='utf-8') as file:
+        reader = list(csv.DictReader(file))
+        page_number = int(request.GET.get('page', 1))
+        paginator = Paginator(reader, 10)
+        page = paginator.get_page(page_number)
+        context = {
+            'bus_stations': page,
+            'page': page,
+        }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -19,12 +20,38 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def omlet(request):
+    ingredient_number = int(request.GET.get("servings", 1))
+    context = {
+        'omlet': {
+            'яйца, шт': 2 * ingredient_number,
+            'молоко, л': 0.1 * ingredient_number,
+            'соль, ч.л.': 0.5 * ingredient_number,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta(request):
+    ingredient_number = int(request.GET.get("servings", 1))
+    context = {
+        'pasta': {
+            'макароны, г': 0.3 * ingredient_number,
+            'сыр, г': 0.05 * ingredient_number,
+        }
+    }
+    return render(request, 'calculator/index1.html', context)
+
+
+def buter(request):
+    ingredient_number = int(request.GET.get("servings", 1))
+    context = {
+        'buter': {
+            'хлеб, ломтик': 1 * ingredient_number,
+            'колбаса, ломтик': 1 * ingredient_number,
+            'сыр, ломтик': 1 * ingredient_number,
+            'помидор, ломтик': 1 * ingredient_number,
+        },
+    }
+    return render(request, 'calculator/index2.html', context)
