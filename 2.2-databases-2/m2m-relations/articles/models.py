7bc8581ca8242048b60c9c31225a389193792a74
Q1unt <dima.kunitsien.03@gmail.com>
2023-10-28 23:47:32 +0300

    Finalization of task 2.2

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Тэг')
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):
+    article = models.ForeignKey('Article', on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name="основной")
+
+    class Meta:
+        verbose_name = "тег статьи"
+        verbose_name_plural = "теги статьи"
+        ordering = ['is_main']
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
