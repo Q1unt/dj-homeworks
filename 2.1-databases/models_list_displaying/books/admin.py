1419c3454a977b80709a85d15c7b422c5e6f92b4
Q1unt <dima.kunitsien.03@gmail.com>
2023-10-28 23:53:29 +0300

    Finalized the admin panel

--- 2.1-databases/models_list_displaying/books/admin.py
+++ 2.1-databases/models_list_displaying/books/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 
-from books.models import Book
+from .models import Article, Scope, Tag
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'pub_date',)
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            try:
+                if form.cleamed_data["is_main"]:
+                    count += 1
+            except KeyError:
+                pass
+        if count > 1:
+            raise ValidationError('Основным может быть один ТЕГ')
+        return super().clean()
 
 
-admin.site.register(Book, BookAdmin)
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 3
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    pass
